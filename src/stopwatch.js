0b2a8c2e5dc521f8452fd8f6eb9d61f93d3dc734
Day Vazquez <dayvazquez@protonmail.com>
2022-09-25 21:28:08 +0200

    Create stopwatch.js

---
+++ src/stopwatch.js
@@ -0,0 +1,62 @@
+let running = 0;
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
+let time = document.getElementById('time');
+
+function startTime() {
+    if (running === 0) {
+        running = 1;
+        increment();
+        start.innerHTML = 'Pause';
+    } else {
+        running = 0;
+        start.innerHTML = 'Resume';
+    }
+}
+
+function stopTime() {
+    running = 0;
+    start.innerHTML = 'Start';
+}
+
+function resetTime() {
+    running = 0;
+    time.innerHTML = '00:00:00';
+    start.innerHTML = 'Start';
+}
+
+function increment() {
+    if (running === 1) {
+        setTimeout(function() {
+            time.innerHTML = getTime();
+            increment();
+        }, 1000);
+    }
+}
+
+function getTime() {
+    let currentTime = new Date();
+    let timeElapsed = new Date(currentTime - startTime);
+    let min = timeElapsed.getMinutes();
+    let sec = timeElapsed.getSeconds();
+    let ms = timeElapsed.getMilliseconds();
+    return (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec) + ':' + (ms < 10 ? '00' + ms : ms < 100 ? '0' + ms : ms);
+}
+
+start.onclick = function() {
+    if (running === 0) {
+        startTime = new Date();
+        startTime();
+    } else {
+        stopTime();
+    }
+};
+
+stop.onclick = function() {
+    stopTime();
+};
+
+reset.onclick = function() {
+    resetTime();
+};
